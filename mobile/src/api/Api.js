4f8606121346384bf7f4898134f38f046fdb857f
Emanuel Quimper <quimperemanuel@gmail.com>
2018-08-23 06:41:08 -0400

    End of part 8.5

--- mobile/src/api/Api.js
+++ mobile/src/api/Api.js
@@ -1,3 +1,5 @@
 import wretch from 'wretch';
 
-export const customersApi = wretch('http://localhost:3000/api/v1/customers');
+import { BASE_URL } from '../constants';
+
+export const customersApi = wretch(`${BASE_URL}/customers`);
---
+++ mobile/src/constants/index.js
@@ -0,0 +1,1 @@
+export const BASE_URL = 'http://192.168.1.65:3000/api/v1';
